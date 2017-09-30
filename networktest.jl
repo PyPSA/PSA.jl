d4dcb44f90008316aff62dec80b32b2129d48aba
Fabian <fab.hof@gmx.de>
2017-09-30 18:24:00 +0200

    add min_up_time and min down time for generators, still problems

--- networktest.jl
+++ networktest.jl
@@ -6,15 +6,14 @@ using JuPSA
 
 
 
-network = import_network("mininetwork")
-network.loads_t["p"] = DataFrame(My_bus_0 = 0, My_bus_1 = 0, My_bus_2 = 100)
+# network = import_network("mininetwork")
+# network.loads_t["p"] = DataFrame(My_bus_0 = 0, My_bus_1 = 0, My_bus_2 = 100)
 
-# network = JuPSA.import_network("pre8-37")
-# network.loads_t["p"][:LT0_0] =0
-# network.generators = network.generators[network.generators[:carrier].=="OCGT",:]
-# network.generators[:p_nom] = network.generators[:p_nom] *100
-# network.loads_t["p"] = network.loads_t["p"][1:1000,:]
-
+network = JuPSA.import_network("pre8-37")
+network.loads_t["p"][:LT0_0] =0
+network.generators = network.generators[network.generators[:carrier].=="OCGT",:]
+network.generators[:p_nom] = network.generators[:p_nom] *100
+network.loads_t["p"] = network.loads_t["p"][1:100,:]
 
 idx(dataframe) = Dict(zip(dataframe[:name], Iterators.countfrom(1)))
 rev_idx(dataframe) = Dict(zip(Iterators.countfrom(1), dataframe[:name]))
@@ -28,7 +27,12 @@ JuPSA.calculate_dependent_values(network)
 # network.lines[1, :extendable] = true
 # network.lines[1, :s_nom_min] = 0
 # network.lines[1, :s_nom_max] = 1000
-
+network.generators[1,:commitable] = true
+network.generators[:min_up_time] = 0
+network.generators[1,:min_up_time] = 5
+network.generators[:min_down_time] = 0
+network.generators[1,:min_down_time] = 5
+network.generators[:initial_status] = true
 
 buses = network.buses
 
@@ -49,9 +53,7 @@ if length(network.lines)>0
     lines_ext = network.lines[network.lines[:extendable],:]
     lines = [lines_fix; lines_ext]
 else
-    lines_fix = network.lines
-    lines_ext = network.lines
-    lines = [lines_fix; lines_ext]
+    lines_fix = network.lines; lines_ext = network.lines; lines = network.lines
 end
 
 if length(network.links)>0
@@ -59,9 +61,7 @@ if length(network.links)>0
     links_ext = network.links[network.links[:extendable],:]
     links = [links_fix; links_ext]
 else
-    links_fix = network.links
-    links_ext = network.links
-    links = [links_fix; links_ext]
+    links_fix = network.links; links_ext = network.links; links = network.links
 end
 
 for df in [buses, generators_fix, generators_ext, generators_com, lines_fix, lines_ext,
@@ -90,10 +90,15 @@ m = Model(solver=GurobiSolver())
 
     (generators_fix[gr, :p_nom].*generators_fix[gr, :p_min_pu] <= g_fix[gr=1:G_fix,t=1:T]
                             <= generators_fix[gr, :p_nom].*generators_fix[gr, :p_max_pu])
-    g_ext[gr=1:G_ext,t=1:T]
-    generators_ext[gr, :p_nom_min]          <=  g_p_nom[gr=1:G_ext]      <= generators_ext[gr, :p_nom_max]
 
-    -lines_fix[l, :s_nom]                       <=  ln_fix[l=1:LN_fix,t=1:T]        <= lines_fix[l, :s_nom]
+    g_ext[gr=1:G_ext,t=1:T]
+    generators_ext[gr, :p_nom_min]          <=  gen_p_nom[gr=1:G_ext]      <= generators_ext[gr, :p_nom_max]
+
+    g_status[gr=1:G_com,t=1:T], Bin
+    (generators_com[gr, :p_nom].*generators_com[gr, :p_min_pu] <= g_com[gr=1:G_com,t=1:T]
+                            <= generators_com[gr, :p_nom].*generators_com[gr, :p_max_pu])
+
+    -lines_fix[l, :s_nom]                   <=  ln_fix[l=1:LN_fix,t=1:T]        <= lines_fix[l, :s_nom]
     ln_ext[l=1:LN_ext,t=1:T]
     lines_ext[l, :s_nom_min]                <=  ln_s_nom[l=1:LN_ext]     <= lines_ext[l, :s_nom_max]
 
@@ -106,11 +111,31 @@ end
 
 ln = [ln_fix; ln_ext]
 lk = [lk_fix; lk_ext]
-gn = [g_fix; g_ext]
+gn = [g_fix; g_ext; g_com]
+g_up_time_i = generators_com[generators_com[:min_up_time].>0, :idx]
+g_down_time_i = generators_com[generators_com[:min_down_time].>0, :idx]
 
 @constraints(m, begin
-        [gr=1:G_ext,t=1:T], g_ext[gr,t] >= g_p_nom[gr].*generators_ext[gr, :p_min_pu]
-        [gr=1:G_ext,t=1:T], g_ext[gr,t] <= g_p_nom[gr].*generators_ext[gr, :p_max_pu]
+        [gr=1:G_ext,t=1:T], g_ext[gr,t] >= gen_p_nom[gr].*generators_ext[gr, :p_min_pu]
+        [gr=1:G_ext,t=1:T], g_ext[gr,t] <= gen_p_nom[gr].*generators_ext[gr, :p_max_pu]
+
+        [gr=1:G_com,t=1:T], g_com[gr,t] - g_com[gr,t].*g_status[gr,t] == 0
+
+        [gr=g_up_time_i,t=1], (sum(g_status[gr,j] for j=t:min.(t+generators_com[gr,:min_up_time]-1,T))
+                            >=
+                            # generators_com[gr,:min_up_time].*g_status[gr,t]
+                            generators_com[gr,:min_up_time].*generators_com[gr, :initial_status])
+        [gr=g_up_time_i,t=2:T], (sum(g_status[gr,j] for j=t:min.(t+generators_com[gr,:min_up_time]-1,T))
+                            >= generators_com[gr,:min_up_time].*g_status[gr,t]
+                            - generators_com[gr,:min_up_time].*g_status[gr,t-1])
+
+        [gr=g_down_time_i,t=1], (generators_com[gr,:min_down_time]
+                            - sum(g_status[gr,j] for j=t:min.(t+generators_com[gr,:min_down_time]-1,T))
+                            >= (- generators_com[gr,:min_down_time].*g_status[gr,t]
+                            + generators_com[gr,:min_down_time].*generators_com[gr, :initial_status]))
+        [gr=g_down_time_i,t=2:T], (sum(g_status[gr,j] for j=t:min.(t+generators_com[gr,:min_down_time]-1,T))
+                            >= - generators_com[gr,:min_down_time].*g_status[gr,t]
+                            + generators_com[gr,:min_down_time].*g_status[gr,t-1])
 
         [l=1:LN_ext,t=1:T], ln_ext[l,t] <=  ln_s_nom[l]
         [l=1:LN_ext,t=1:T], ln_ext[l,t] >= -ln_s_nom[l]
@@ -167,4 +192,4 @@ end
 
 
 solve(m)
-@show getvalue(ln)
+# @show getvalue(ln)

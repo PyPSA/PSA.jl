ce6fbc8fdba255194e4d36655ecebc9acd74e9bf
Fabian <fab.hof@gmx.de>
2017-10-04 16:45:42 +0200

    Fix lopf, restructure code

--- networktest.jl
+++ networktest.jl
@@ -1,13 +1,13 @@
-using DataFrames, JuMP, LightGraphs, Query, Gurobi, GraphLayout
-
+using DataFrames, JuMP, LightGraphs, Gurobi, PyCall
 using JuPSA
 
-
+@pyimport networkx as nx
 
 
 
 # network = import_network("mininetwork")
 # network.loads_t["p"] = DataFrame(My_bus_0 = 0, My_bus_1 = 0, My_bus_2 = 100)
+# network.lines[3, :bus0], network.lines[3, :bus1] = network.lines[3, :bus1], network.lines[3, :bus0]
 
 network = JuPSA.import_network("pre8-37")
 network.loads_t["p"][:LT0_0] =0
@@ -15,24 +15,19 @@ network.generators = network.generators[network.generators[:carrier].=="OCGT",:]
 network.generators[:p_nom] = network.generators[:p_nom] *100
 network.loads_t["p"] = network.loads_t["p"][1:100,:]
 
-idx(dataframe) = Dict(zip(dataframe[:name], Iterators.countfrom(1)))
-rev_idx(dataframe) = Dict(zip(Iterators.countfrom(1), dataframe[:name]))
-
-index_name(dataframe, names) = dataframe[findin(dataframe[:name], names),:]
-index_by(dataframe, col, values) = dataframe[findin(dataframe[col], values),:]
-idx_by(dataframe, col, values) = index_by(dataframe, col, values)[:idx]
-
-
 JuPSA.calculate_dependent_values(network)
 # network.lines[1, :extendable] = true
 # network.lines[1, :s_nom_min] = 0
 # network.lines[1, :s_nom_max] = 1000
-network.generators[1,:commitable] = true
+# network.generators[1,:commitable] = true
 network.generators[:min_up_time] = 0
-network.generators[1,:min_up_time] = 5
+# network.generators[1,:min_up_time] = 5
 network.generators[:min_down_time] = 0
-network.generators[1,:min_down_time] = 5
+# network.generators[1,:min_down_time] = 5
 network.generators[:initial_status] = true
+
+# lopf(network)
+
 
 buses = network.buses
 
@@ -64,8 +59,8 @@ else
     links_fix = network.links; links_ext = network.links; links = network.links
 end
 
-for df in [buses, generators_fix, generators_ext, generators_com, lines_fix, lines_ext,
-    links_fix, links_ext]
+for df in [buses, generators_fix, generators_ext, generators_com, generators,
+     lines_fix, lines_ext, links_fix, links_ext, lines]
     df[:idx] = 1:nrow(df)
 end
 
@@ -121,21 +116,21 @@ g_down_time_i = generators_com[generators_com[:min_down_time].>0, :idx]
 
         [gr=1:G_com,t=1:T], g_com[gr,t] - g_com[gr,t].*g_status[gr,t] == 0
 
-        [gr=g_up_time_i,t=1], (sum(g_status[gr,j] for j=t:min.(t+generators_com[gr,:min_up_time]-1,T))
-                            >=
-                            # generators_com[gr,:min_up_time].*g_status[gr,t]
-                            generators_com[gr,:min_up_time].*generators_com[gr, :initial_status])
-        [gr=g_up_time_i,t=2:T], (sum(g_status[gr,j] for j=t:min.(t+generators_com[gr,:min_up_time]-1,T))
-                            >= generators_com[gr,:min_up_time].*g_status[gr,t]
-                            - generators_com[gr,:min_up_time].*g_status[gr,t-1])
-
-        [gr=g_down_time_i,t=1], (generators_com[gr,:min_down_time]
-                            - sum(g_status[gr,j] for j=t:min.(t+generators_com[gr,:min_down_time]-1,T))
-                            >= (- generators_com[gr,:min_down_time].*g_status[gr,t]
-                            + generators_com[gr,:min_down_time].*generators_com[gr, :initial_status]))
-        [gr=g_down_time_i,t=2:T], (sum(g_status[gr,j] for j=t:min.(t+generators_com[gr,:min_down_time]-1,T))
-                            >= - generators_com[gr,:min_down_time].*g_status[gr,t]
-                            + generators_com[gr,:min_down_time].*g_status[gr,t-1])
+        # [gr=g_up_time_i,t=1], (sum(g_status[gr,j] for j=t:min.(t+generators_com[gr,:min_up_time]-1,T))
+        #                     >=
+        #                     # generators_com[gr,:min_up_time].*g_status[gr,t]
+        #                     generators_com[gr,:min_up_time].*generators_com[gr, :initial_status])
+        # [gr=g_up_time_i,t=2:T], (sum(g_status[gr,j] for j=t:min.(t+generators_com[gr,:min_up_time]-1,T))
+        #                     >= generators_com[gr,:min_up_time].*g_status[gr,t]
+        #                     - generators_com[gr,:min_up_time].*g_status[gr,t-1])
+        #
+        # [gr=g_down_time_i,t=1], (generators_com[gr,:min_down_time]
+        #                     - sum(g_status[gr,j] for j=t:min.(t+generators_com[gr,:min_down_time]-1,T))
+        #                     >= (- generators_com[gr,:min_down_time].*g_status[gr,t]
+        #                     + generators_com[gr,:min_down_time].*generators_com[gr, :initial_status]))
+        # [gr=g_down_time_i,t=2:T], (sum(g_status[gr,j] for j=t:min.(t+generators_com[gr,:min_down_time]-1,T))
+        #                     >= - generators_com[gr,:min_down_time].*g_status[gr,t]
+        #                     + generators_com[gr,:min_down_time].*g_status[gr,t-1])
 
         [l=1:LN_ext,t=1:T], ln_ext[l,t] <=  ln_s_nom[l]
         [l=1:LN_ext,t=1:T], ln_ext[l,t] >= -ln_s_nom[l]
@@ -166,30 +161,60 @@ to_symbol(str) = Symbol(replace(str, " ", "_"))
       == 0 ))
 
 
-
 # Kirchhoff Voltage Law
-for (branch, var, attribute) in [(lines, ln, :x), (links, lk, :r)]
-    g = DiGraph(length(busidx))
-    for l = eachrow(branch)
-      add_edge!(g, busidx[l[:bus0]], busidx[l[:bus1]])
-    end
-    cycles = [i for i in simplecycles(g) if length(i)>2]
-    if attribute==:x
-        @constraint(m, line_cycle_constraint[c=1:length(cycles), t=1:T] ,
-                sum(dot(lines[cycles[c], :x]/380. , ln[cycles[c],t])) == 0)
-    end
-    if attribute==:r
-        @constraint(m, link_cycle_constraint[c=1:length(cycles), t=1:T] ,
-                sum(dot(links[cycles[c], :r]/380. , lk[cycles[c],t])) == 0)
+
+for (branch, var, attribute) in [(lines, ln, :x)]#, (links, lk, :r)]
+    g = nx.Graph()
+    g[:add_nodes_from](busidx)
+    g[:add_edges_from]([(busidx[l[:bus0]], busidx[l[:bus1]]) for l in eachrow(lines)])
+    cycles = [cycle for cycle in nx.cycle_basis(g) if length(cycle)>2]
+    if length(cycles)>0
+        cycles_branch = Array{Int64,1}[]
+        directions = Array{Float64,1}[]
+        for cyc=1:length(cycles)
+            push!(cycles_branch,Int64[])
+            push!(directions,Float64[])
+            for bus=1:length(cycles[cyc])
+                bus0 = cycles[cyc][bus]
+                if bus == length(cycles[cyc])
+                    bus1 = cycles[cyc][1]
+                else
+                    bus1 = cycles[cyc][bus+1]
+                end
+                try
+                    push!(cycles_branch[cyc], branch[((branch[:bus0].==reverse_busidx[bus0])
+                                .&(branch[:bus1].==reverse_busidx[bus1])),:idx][1] )
+                    push!(directions[cyc], 1.)
+                catch y
+                    if isa(y, BoundsError)
+                        push!(cycles_branch[cyc], branch[((branch[:bus0].==reverse_busidx[bus1])
+                                        .&(branch[:bus1].==reverse_busidx[bus0])),:idx][1] )
+                        push!(directions[cyc], -1.)
+                    else
+                        return y
+                    end
+                end
+            end
+        end
+        if attribute==:x
+            @constraint(m, line_cycle_constraint[c=1:length(cycles_branch), t=1:T] ,
+                    sum(dot(directions[c] .* lines[cycles_branch[c], :x]/380. , ln[cycles_branch[c],t])) == 0)
+        end
+        if attribute==:r
+            @constraint(m, link_cycle_constraint[c=1:length(cycles_branch), t=1:T] ,
+                    sum(dot(directions[c] .* links[cycles_branch[c], :r]/380. , lk[cycles_branch[c],t])) == 0)
+        end
     end
 end
+
 # Might be nessecary to loop over all subgraphs as
 # for (sn, sub) in enumerate(weakly_connected_components(g))
 #     # g_sub = induced_subgraph(g, sub)[1]
 
 
+
 @objective(m, Min, sum(dot(generators[:marginal_cost], gn[:,t]) for t=1:T))
 
 
-solve(m)
-# @show getvalue(ln)
+# solve(m)
+# getvalue(ln)
